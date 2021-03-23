b01852d4a9cffbd25f080a538c7de48f6abae9f5
Patrick Shih <patrick-shih@hsopto.com>
2021-03-23 14:40:47 +0800

    add: Add Repository class.

---
+++ Uplift.DataAccess/Data/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Uplift.DataAccess.Data.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        T Get(int id);
+        
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            string includeProperties = null);
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null, 
+            string includeProperties = null);
+        
+        void Remove(int id);
+        void Remove(T entity);
+    }
+}
