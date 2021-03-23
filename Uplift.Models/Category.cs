d712ee4bf9591522d9672f393ce322ef16b5488e
Patrick Shih <patrick-shih@hsopto.com>
2021-03-23 14:33:44 +0800

    add: Add Category Model.

---
+++ Uplift.Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uplift.Models
+{
+    public class Category
+    {
+       [Key]
+       public int Id { get; set; }
+
+       [Required]
+       [Display(Name = "Category Name")]
+       public string Name { get; set; }
+
+       public int DisplayOrder { get; set; }
+    }
+}
