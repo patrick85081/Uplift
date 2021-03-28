bffed5ab7b72442266812f80de1a9bd32e4a2c16
Patrick Shih <patrick-shih@hsopto.com>
2021-03-28 13:28:39 +0800

    add: Add Servie Controller and Index View

--- Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
 
 namespace Uplift.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ServiceController : Controller
     {
+        private IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _hostEnvironment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+
         public IActionResult Index()
         {
-            throw new System.NotImplementedException();
+            return View();
+        }
+
+        #region API Calls
+
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _unitOfWork.Service.GetAll(includeProperties: "Category,Frequency")});
+        }
+
+        #endregion
+
+        public IActionResult Upsert()
+        {
+            return View();
         }
     }
 }
