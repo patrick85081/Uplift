933b1125f5937bd4e07d67e4336a3a203bc2c2b1
Patrick Shih <patrick-shih@hsopto.com>
2021-03-28 13:49:39 +0800

    add: Add ServieVM

---
+++ Uplift.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Uplift.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+        public IEnumerable<SelectListItem> FrequencyList { get; set; }
+    }
+}
