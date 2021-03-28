8cb51dfd8bfcd49258879398a080a18f0121f17d
Patrick Shih <patrick-shih@hsopto.com>
2021-03-28 14:26:04 +0800

    add: Add Service Update Function

--- Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+using Uplift.Models.ViewModels;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -16,9 +20,92 @@ namespace Uplift.Areas.Admin.Controllers
             _hostEnvironment = hostEnvironment;
         }
 
+        [BindProperty]
+        public ServiceVM ServVM { get; set; }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            ServVM = new ServiceVM()
+            {
+                Service = new Models.Service(),
+                CategoryList = _unitOfWork.Category.GetCategoryListForDropDown(),
+                FrequencyList = _unitOfWork.Frequency.GetFrequencyListForDropDown(),
+            };
+            if (id != null)
+            {
+                ServVM.Service = _unitOfWork.Service.Get(id.GetValueOrDefault());
+            }
+
+            return View(ServVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (ServVM.Service.Id == 0)
+                {
+                    //New Service
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\services");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+
+                    ServVM.Service.ImageUrl = @"\images\services\{fileName}{extension}";
+
+                    _unitOfWork.Service.Add(ServVM.Service);
+                }
+                else
+                {
+                    //Edit Service
+                    var serviceFromDb = _unitOfWork.Service.Get(ServVM.Service.Id);
+                    if (files.Count > 0)
+                    {
+                        string fileName = Guid.NewGuid().ToString();
+                        var uploads = Path.Combine(webRootPath, @"images\services");
+                        var extension_new = Path.GetExtension(files[0].FileName);
+
+                        var imagePath = Path.Combine(webRootPath, serviceFromDb.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+
+                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension_new), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStreams);
+                        }
+                        ServVM.Service.ImageUrl = @"\images\services\{fileName}{extension_new}";
+                    }
+                    else
+                    {
+                        ServVM.Service.ImageUrl = serviceFromDb.ImageUrl;
+                    }
+
+                    _unitOfWork.Service.Update(ServVM.Service);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ServVM.CategoryList = _unitOfWork.Category.GetCategoryListForDropDown();
+                ServVM.FrequencyList = _unitOfWork.Frequency.GetFrequencyListForDropDown();
+                return View(ServVM);
+            }
         }
 
         #region API Calls
@@ -29,10 +116,5 @@ namespace Uplift.Areas.Admin.Controllers
         }
 
         #endregion
-
-        public IActionResult Upsert()
-        {
-            return View();
-        }
     }
 }
