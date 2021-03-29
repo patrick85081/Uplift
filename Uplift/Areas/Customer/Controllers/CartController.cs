0624964aaef6eb00b29afae81045e620c12cd40a
Patrick Shih <patrick-shih@hsopto.com>
2021-03-29 16:31:26 +0800

    add: Add Cart Index View

--- Uplift/Areas/Customer/Controllers/CartController.cs
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -36,8 +36,20 @@ namespace Uplift.Controllers
                     CartVM.ServiceList.Add(unitOfWork.Service
                         .GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
                 }
+                return View(CartVM);
             }
-            return View(CartVM);
+
+            return RedirectToAction("Index", "Home");
+        }
+        
+        public IActionResult Remove(int serviceId)
+        {
+            List<int> sessionList = new List<int>();
+            sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+            sessionList.Remove(serviceId);
+            HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
