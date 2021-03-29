0d1460ce6bcb301f597f1c6db9f6a81ab8017b0e
Patrick Shih <patrick-shih@hsopto.com>
2021-03-29 16:16:09 +0800

    add: Add CartController

---
+++ Uplift.Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Uplift.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public IList<Service> ServiceList { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+    }
+}
---
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Extensions;
+using Uplift.Models;
+using Uplift.Models.ViewModels;
+using Uplift.Utility;
+
+namespace Uplift.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        [BindProperty]
+        public CartViewModel CartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+            CartVM = new CartViewModel()
+            {
+                OrderHeader = new OrderHeader(),
+                ServiceList = new List<Service>()
+            };
+        }
+        
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.IsExist(SD.SessionCart))
+            {
+                var sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                foreach (var serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(unitOfWork.Service
+                        .GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
+                }
+            }
+            return View(CartVM);
+        }
+    }
+}
